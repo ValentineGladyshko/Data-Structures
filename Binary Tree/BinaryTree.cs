5501c710d25a75caf63b293304ef6284ca41a113
Valentine Gladyshko <valentine9022@gmail.com>
2022-01-03 01:20:35 +0200

    Almost written unbalanced binary search tree

---
+++ Binary Tree/BinaryTree.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace DataStructures
+{
+
+
+	public class BinaryTree
+	{
+		public BinaryTree()
+		{ }
+		public BinaryTree(int value)
+		{
+			Value = value;
+		}
+		public int? Value { get; set; }
+		public BinaryTree? RightNode { get; set; }
+		public BinaryTree? LeftNode { get; set; }
+
+		public void Insert(int value)
+		{
+			if (Value == null)
+			{
+				Value = value;
+			}
+			else if (value < Value)
+			{
+				if (RightNode == null)
+					RightNode = new BinaryTree(value);
+				else RightNode.Insert(value);
+			}
+			else if (value > Value)
+			{
+				if (LeftNode == null)
+					LeftNode = new BinaryTree(value);
+				else LeftNode.Insert(value);
+			}
+			else
+			{ }
+		}
+
+		public bool Find(int value)
+		{
+			if (Value == null)
+			{
+				return false;
+			}
+			if (Value == value)
+			{
+				return true;
+			}
+			else if (value < Value)
+			{
+				if (RightNode == null)
+					return false;
+				else RightNode.Find(value);
+			}
+			else
+			{
+				if (LeftNode == null)
+					return false;
+				else LeftNode.Find(value);
+			}
+			return false;
+		}
+
+		public void InfixTraverse()
+		{
+			if (Value == null)
+			{
+				return;
+			}
+
+			if (RightNode != null)
+			{
+				RightNode.InfixTraverse();
+			}	
+
+			Console.Write(Value);
+			Console.Write(" ");
+
+			if (LeftNode != null)
+			{
+				LeftNode.InfixTraverse();
+			}
+		}
+
+		public void PrefixTraverse()
+		{
+			if (Value == null)
+			{
+				return;
+			}
+
+			Console.Write(Value);
+			Console.Write(" ");
+
+			if (RightNode != null)
+			{
+				RightNode.PrefixTraverse();
+			}		
+
+			if (LeftNode != null)
+			{
+				LeftNode.PrefixTraverse();
+			}
+		}
+
+		public void PostfixTraverse()
+		{
+			if (Value == null)
+			{
+				return;
+			}
+
+			if (RightNode != null)
+			{
+				RightNode.PostfixTraverse();
+			}
+
+			if (LeftNode != null)
+			{
+				LeftNode.PostfixTraverse();
+			}
+
+			Console.Write(Value);
+			Console.Write(" ");
+		}
+
+		public void Traverse()
+		{
+			if (Value == null)
+			{
+				return;
+			}
+
+			Console.Write(Value);
+			Console.Write(" ");
+			if (RightNode != null)
+			{
+				Console.Write("right: ");
+				Console.Write(RightNode.Value);
+				Console.Write(" ");
+			}
+			if (LeftNode != null)
+			{
+				Console.Write("left: ");
+				Console.Write(LeftNode.Value);
+				Console.Write(" ");
+			}
+			Console.WriteLine(" ");
+
+			if (RightNode != null)
+			{
+				RightNode.Traverse();
+			}
+
+			if (LeftNode != null)
+			{
+				LeftNode.Traverse();
+			}
+
+			
+		}
+
+	}
+}
---
+++ Binary Tree/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DataStructures;
+
+BinaryTree binaryTree = new BinaryTree();
+Random random = new Random();
+for (int i = 0; i < 100; i++)
+{
+    binaryTree.Insert(random.Next(100));
+}
+binaryTree.Traverse();
+Console.WriteLine();
+binaryTree.InfixTraverse();
+Console.WriteLine();   
+binaryTree.PrefixTraverse();
+Console.WriteLine();
+binaryTree.PostfixTraverse();
